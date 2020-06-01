c552277e022dfe1d176d839a1874f8147257fe90
Megha Ilango <meghailango@Meghas-MacBook-Pro.local>
2020-05-31 23:19:19 -0700

    reward hope this works

--- train_dqn.py
+++ train_dqn.py
@@ -117,7 +117,12 @@ def train(
 
         all_traj = dataset.get_trajectories()
         q_difference = q_diff(dqn, all_traj)
-        undiscounted_avg_reward = sum([(sum(sarsa[2] for traj in trajectories )/len(traj)) for traj in all_traj])/len(all_traj)
+
+        avgs = []
+        for traj in all_traj:
+            avg_reward = sum([sarsa[2] for sarsa in traj])/len(traj)
+            avgs.append(avg_reward)
+        undiscounted_avg_reward = sum(avgs)/len(avgs)
 
         writer.add_scalar("QDiff", q_difference)
         writer.add_scalar("AvgReward", undiscounted_avg_reward) #calculate this reward

fixed loss gradients

train_dqn.py
@@ -28,13 +28,11 @@ def compute_loss(s, a, r, s_prime, dqn, discount_factor, dqn_prime=None):
     q = dqn.forward(s)[torch.arange(N), a.long()]
     if dqn_prime: # using ddqn and target network
         bootstrap = dqn_prime.forward(s_prime)[torch.arange(N), dqn.forward_best_actions(s_prime)[0]]
-        target = r + discount_factor * bootstrap
     else:
         bootstrap = dqn.forward_best_actions(s_prime)[1]
-        target = r + discount_factor * bootstrap
-
-    target.detach() # do not propogate graidents through target
-    return F.mse_loss(q, target)
+    target = r + discount_factor * bootstrap
+    target = target.detach() # do not propogate graadients through targets
+    return F.mse_loss(q, target.float())
 
 def train(
     learning_rate=0.00025,